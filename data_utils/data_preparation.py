dce595d8c7da8377ad8e1811975fb17c76cadec2
Aliaksandr Shcharbitski <aliaksandr.shcharbitski@gmail.com>
2024-05-21 11:54:49 +0200

    Add CIFAR dataset

--- data_utils/data_preparation.py
+++ data_utils/data_preparation.py
@@ -1,10 +1,18 @@
 import zipfile
+import argparse
 
 def unzip_dataset(inputpath: str, outputpath: str):
     with zipfile.ZipFile(inputpath, "r") as zipped_file:
         zipped_file.extractall(outputpath)
 
+
+
 if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dataset', type=str, default="data/cifar10c.zip", help='path to csv')
+    args = parser.parse_args()
+
     print("Extraction started")
-    unzip_dataset("data/cmnist.zip", "data")
+    unzip_dataset(args.dataset, "data")
     print("Extraction ended")
